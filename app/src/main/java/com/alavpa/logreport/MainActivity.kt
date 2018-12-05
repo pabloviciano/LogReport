90a622ff4e48c9e9513644083f91feab51031157
Alex Avila <alex.avila@gigigo.com>
2018-12-05 14:23:48 +0100

    New: Compile in 27

--- app/src/main/java/com/alavpa/logreport/MainActivity.kt
+++ app/src/main/java/com/alavpa/logreport/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.alavpa.logreport
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.alavpa.logger.LogActivity
 import com.alavpa.logger.Logger
 
 class MainActivity : BaseActivity() {
@@ -9,5 +11,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //startActivity(Intent(this, LogActivity::class.java))
     }
 }
